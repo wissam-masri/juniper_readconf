da693bfe132258e2a51e94e6354816f07e6b2203
Wissam Masri <masri.wissam@gmail.com>
2011-09-06 14:55:46 +0200

    first commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+This application is developped by Wissam MASRI in context of an internship at Orange LABS.
+
+It consists in basic exchange of data and commands on a Juniper MX router.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuniperReadconfApp::Application.config.secret_token = '939e356a0afa3e1703afd635a64d14a3a71c17fb8e7f3a9920e3dc48d892f22cc3b932818fb823bf442b3b2585113893a697616f243aeba2f09c719e0e0ef4b0'
